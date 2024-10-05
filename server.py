ce917c1e4733b17701e83da00be3327f3e7820fc
sofaspawn <h.m1nus.work@gmail.com>
2024-10-05 09:24:35 +0530

    serving files

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def helloworld():
-    return "<h1>Hello World</h1>"
+def landing():
+    f = open("./static/index.html")
+    return f.read()
